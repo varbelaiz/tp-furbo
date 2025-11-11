d4d132f7eb530ee34c0ee0215807f4801a61f878
anatissera <atissera@udesa.edu.ar>
2025-11-11 16:10:41 -0300

    chau dataset

--- download_data.py
+++ download_data.py
@@ -2,10 +2,7 @@
 
 import SoccerNet
 from SoccerNet.Downloader import SoccerNetDownloader
-downloader =SoccerNetDownloader(LocalDirectory="path/to/SoccerNet")
 
-downloader.downloadGames(files=["Labels-v2.json"], split=["train","valid","test"])
-
-# optional: download ResNet features or Baidu features
-downloader.downloadGames(files=["1_ResNET_TF2_PCA512.npy", "2_ResNET_TF2_PCA512.npy"],
-                         split=["train","valid","test","challenge"])
+downloader = SoccerNetDownloader(LocalDirectory="path/to/SoccerNet")
+downloader.downloadDataTask(task="tracking", split=["train","test","challenge"])
+downloader.downloadDataTask(task="tracking-2023", split=["train", "test", "challenge"])
