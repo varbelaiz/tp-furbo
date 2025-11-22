0551d494316243b6a7e6c78cd0b2a43394a7a497
Amato de Lusarreta, Franco <famatodelusarreta@udesa.edu.ar>
2025-11-22 19:21:01 -0300

    smaller yolo

--- modules/homografy/train.py
+++ modules/homografy/train.py
@@ -4,7 +4,7 @@ import wandb
 
 def main():
     # --- CONFIGURACIÓN ---
-    MODEL_NAME = 'yolov8x-pose.pt'
+    MODEL_NAME = 'yolov8n-pose.pt'
     DATA_YAML = 'soccernet.yaml'
     EPOCHS = 100
     IMGSZ = 640
